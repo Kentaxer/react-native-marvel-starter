a3d850b0888e2301ec55d0695646a5dced9bcc3c
Kenny <i_wangyuyue@hotmail.com>
2018-04-06 14:06:02 +0530

    Support image

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,7 +6,8 @@ import {
     Text,
     View,
     ScrollView,
-    Button
+    Button,
+    Image,
 } from 'react-native';
 
 export default class Home extends Component {
@@ -19,13 +20,27 @@ export default class Home extends Component {
         let characters = ""
         if(this.props.characters){
             characters = this.props.characters.map((item, index)=>{
-                return <View><Text key={index}>{item.name}</Text></View>
+                console.log(item);
+                return (
+                    <View style={styles.movieItem}>
+                        <Text 
+                        key={index}
+                        style={{ width: 150, height: 30 }}>{item.name}</Text>
+                        <Image
+                            style={{ width: 150, height: 150 }}
+                            source={{ uri: item.thumbnail.path + "." + item.thumbnail.extension }} />
+                    </View>
+                    )
             });
         }
         return (
             <View style={styles.container}>
                 <Text style={styles.header}>Marvel Characters</Text>
-                {characters}
+                <ScrollView>
+                    <View style={styles.movieContainer}>
+                        {characters}
+                    </View>
+                </ScrollView>
             </View>
         );
     }
@@ -36,8 +51,18 @@ const styles = StyleSheet.create({
     container: {
         flex:1,
     },
+
+    movieItem:{
+        margin:10,
+    },
+
+    movieContainer:{
+        flexDirection:"row",
+        flexWrap:"wrap",
+    },
+
     header:{
         fontWeight: "bold",
-        marginBottom:20
+        marginBottom:20,
     }
 })
